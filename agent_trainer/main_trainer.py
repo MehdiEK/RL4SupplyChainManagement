016efd8c6b2bb1c0e31195773559860b54a8a498
MehdiEK <elkansoulimehdi@yahoo.fr>
2024-03-16 18:35:10 +0100

    [trainer] - Update storage + verbose mode

--- agent_trainer/main_trainer.py
+++ agent_trainer/main_trainer.py
@@ -2,13 +2,23 @@
 Main file for training RL agent. 
 
 Creation date: 08/03/2024
-Last modification: 08/02/2024
+Last modification: 08/03/2024
 By: Mehdi EL KANSOULI 
 """
 import numpy as np 
 
 
-def PPOTrainer(agent, env, nb_episode:int, episode_length:int):
+def normalize_obs(obs, suppliers):
+    """
+    """
+    norm_obs = obs.copy()
+    for key, val in obs.items():
+        norm_obs[key] = val / np.array([suppliers[key].get("stock_max"), 150])
+
+    return norm_obs
+
+def PPOTrainer(agent, env, suppliers, nb_episode:int, episode_length:int, 
+               nb_epochs=20):
     """
     This function train a PPO agent. 
     """
@@ -21,31 +31,41 @@ def PPOTrainer(agent, env, nb_episode:int, episode_length:int):
         actions = []
         rewards = []
         old_probs = []
+        profits = []
 
         # initialize environment
         obs, _ = env.reset()
+        norm_obs = normalize_obs(obs, suppliers=suppliers)
+        observations.append(norm_obs)
 
         # start runnnig episode
-        for step in range(episode_length):
+        for _ in range(episode_length):
 
             # get action and save action+obs
-            action = agent.get_action(obs)
-            observations.append(obs)
+            action = agent.get_action(norm_obs)
             actions.append(action)
 
             # compute old prob
-            old_probs.append(agent.get_probs(obs, action))
+            old_probs.append(agent.get_probs(norm_obs, action))
 
             # get new obs and save reward obtained
-            obs, reward, _ = env.step(action)
+            obs, reward, profit = env.step(action)
+            norm_obs = normalize_obs(obs, suppliers=suppliers)
+
             rewards.append(reward)
+            profits.append(profit)
+            observations.append(norm_obs)
 
         # print current perfomance of the agent
         cum_rewards[episode] = np.sum(rewards)
-        print("Cumulative reward: ", cum_rewards[episode])
+        if episode % 100 == 0: 
+            print("\nEpisode: ", episode)
+            print("Sum of rewards: ", cum_rewards[episode])
+            print("Avg profit: ", np.mean(profits))
 
         # train the agent. 
-        agent.train(observations, actions, rewards, old_probs)
+        for _ in range(nb_epochs):
+            agent.train(observations, actions, rewards, old_probs)
     
     return cum_rewards 
 
